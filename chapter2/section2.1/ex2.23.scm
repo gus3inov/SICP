4638d8c793828ac8dfa2ad2ca389ad1de2a6cdcd
Muslim Guseinov <gusejnov1999@list.ru>
2019-10-22 23:41:17 +0300

    add exercise 2.23

---
+++ chapter2/section2.1/ex2.23.scm
@@ -0,0 +1,18 @@
+(define (reverse items)
+  (define (reverse-iter list1 list2)
+    (if (null? list1)
+        list2
+        (reverse-iter (cdr list1) (cons (car list1) list2))))
+  (reverse-iter items (list)))
+
+(define (for-each proc items)
+  (define reversed-items (reverse items))
+  (define (for-each-rec source)
+    (if (null? source)
+        items
+        (proc (car source)
+        (for-each-rec (cdr source)))))
+  (for-each-rec reversed-items))
+
+(for-each (lambda (x) (newline) (display x))
+    (list 57 321 88))
