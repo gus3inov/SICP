3f7419550b1ffe6af2bd39ec3b0ed93b16775c88
Muslim Guseinov <gusejnov1999@list.ru>
2019-11-03 16:54:00 +0300

    fix name attributes 2.27

--- chapter2/section2.1/ex2.27.scm
+++ chapter2/section2.1/ex2.27.scm
@@ -1,6 +1,6 @@
 (define (deep-reverse items)
-  (define (reverse items)
-        (if (list? items) (deep-reverse items) items))
+  (define (reverse item)
+        (if (list? item) (deep-reverse item) item))
   (define (reverse-iter list1 list2)
     (if (null? list1) list2
         (reverse-iter (cdr list1) (cons (reverse (car list1)) list2))))
