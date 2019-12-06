3405d4eacac6722f49e5d1d4317338c192321863
Muslim Guseinov <gusejnov1999@list.ru>
2019-12-07 01:29:52 +0300

    update exercise 2.40

--- chapter2/section2.1/ex2.40.scm
+++ chapter2/section2.1/ex2.40.scm
@@ -1,3 +1,25 @@
+(define nil '())
+
+(define (square x) (* x x))
+
+(define (smallest-divisor n)
+  (find-divisor n 2))
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (+ test-divisor 1)))))
+(define (divides? a b)
+  (= (remainder b a) 0))
+
+(define (prime? n)
+  (= n (smallest-divisor n)))
+
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+    initial
+  (op (car sequence)
+    (accumulate op initial (cdr sequence)))))
+
 (define (enumerate-interval low high)
   (if (> low high)
       nil
@@ -26,3 +48,5 @@
     (filter
       prime-sum?
       (unique-pairs n))))
+
+(display (prime-sum-pairs 6))
