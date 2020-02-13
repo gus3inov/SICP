9cbdf20d2b4f025a73d8e1c9548d9ce72a498c8a
Muslim Guseinov <gusejnov1999@list.ru>
2020-02-13 22:28:04 +0300

    update exercise 2.46

--- chapter2/section2.1/ex2.46.scm
+++ chapter2/section2.1/ex2.46.scm
@@ -3,10 +3,10 @@
 (define (ycor-vect v) (cdr v))
 
 (define (add-vect v1 v2)
-  (cons (+ (xcor-vect v1) (xcor-vect v2)) (+ (ycor-vect v1) (ycor-vect v2))))
+  (make-vect (+ (xcor-vect v1) (xcor-vect v2)) (+ (ycor-vect v1) (ycor-vect v2))))
 
 (define (sub-vect v1 v2)
-  (cons (- (xcor-vect v1) (xcor-vect v2)) (- (ycor-vect v1) (ycor-vect v2))))
+  (make-vect (- (xcor-vect v1) (xcor-vect v2)) (- (ycor-vect v1) (ycor-vect v2))))
 
 (define (scale-vect s v)
-  (cons (* s (xcor-vect v)) (* s (ycor-vect v))))
+  (make-vect (* s (xcor-vect v)) (* s (ycor-vect v))))
