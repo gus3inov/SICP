bac74ce2fe83fc49cf40afe642f5c2e912ba5eb0
Muslim Guseinov <gusejnov1999@list.ru>
2020-03-17 23:53:25 +0300

    add derive selectors

--- chapter2/section2.3/derive.scm
+++ chapter2/section2.3/derive.scm
@@ -26,3 +26,17 @@
 (define (make-sum a1 a2) (list ’+ a1 a2))
 
 (define (make-product m1 m2) (list ’* m1 m2))
+
+(define (sum? x)
+  (and (pair? x) (eq? (car x) ’+)))
+
+(define (addend s) (cadr s))
+
+(define (augend s) (caddr s))
+
+(define (product? x)
+  (and (pair? x) (eq? (car x) ’*)))
+
+(define (multiplier p) (cadr p))
+
+(define (multiplicand p) (caddr p))
