1befc05f63b5ca7d2e954de488e1ce92e48c0dea
Muslim Guseinov <gusejnov1999@list.ru>
2020-03-18 23:47:04 +0300

    add exercise 2.58-a

---
+++ chapter2/section2.3/ex2.58.scm
@@ -0,0 +1,46 @@
+;; 2.58 - a
+
+(define (sum? x)
+  (and (pair? x) (eq? (cadr x) `+)))
+
+(define (make-sum a1 a2)
+  (cond ((=number? a1 0) a2)
+        ((=number? a2 0) a1)
+        ((and (number? a1) (number? a2)) (+ a1 a2))
+        (else (list a1 `+ a2))))
+
+(define (addend s) (car s))
+
+(define (augend s)
+  (if (null? (cdddr s))  
+      (caddr s)            
+      (make-sum (addend (cddr s))
+                (augend (cddr s)))))
+
+(define (product? x)
+  (and (pair? x) (eq? (cadr x) `*)))
+
+(define (make-product m1 m2)
+  (cond ((or (=number? m1 0) (=number? m2 0)) 0)
+        ((=number? m1 1) m2)
+        ((=number? m2 1) m1)
+        ((and (number? m1) (number? m2)) (* m1 m2))
+        (else (list m1 `* m2))))
+
+(define (multiplier p) (car p))
+
+(define (multiplicand p)                          
+  (if (null? (cdddr p))                              
+      (caddr p)                                        
+      (make-product (multiplier (cddr p))     
+                    (multiplicand (cddr p)))))
+
+(define (make-subtract a1 a2)
+  (cond ((=number? a1 0) a2)
+        ((=number? a2 0) a1)
+        ((and (number? a1) (number? a2)) (- a1 a2))
+        (else (list a1 `- a2))))
+
+(deriv `(x + (3 * (x + (y + 2)))) `x)
+
+;; 2.58 - b 
