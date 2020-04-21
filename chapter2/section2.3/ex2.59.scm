e93fea3241276473656b69372e4b58f29408d14f
Muslim Guseinov <gusejnov1999@list.ru>
2020-04-21 23:56:50 +0300

    add exercise 2.59

--- chapter2/section2.3/ex2.59.scm
+++ chapter2/section2.3/ex2.59.scm
@@ -1,6 +1,10 @@
+(define (adjoin-set x set)
+   (if (element-of-set? x set)
+       set
+       (cons x set)))
+
 (define (union-set set1 set2)
-  (cond ((or (null? set1) (null? set2)) `())
-        ((not (element-of-set? (car set1) set2))
-         (cons (car set1)
-               (intersection-set (cdr set1) set2)))
-        (else (intersection-set (cdr set1) set2))))
+  (cond ((and (null? set2) (not (null? set1))) set1)
+        ((and (null? set1) (not (null? set2))) set2)
+        ((and (null? set1) (null? set2)) `())
+        (else (adjoin-set (car set1) (union-set (cdr set1) set2)))))
